0547f66c581290211a0bdfda47c14210c2804f1e
Roy <rhavila789@gmail.com>
2022-09-02 23:11:04 -0500

    feat: add cors to express

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
 
 app.listen(PORT, async () => {
     console.log(`Listen on port ${PORT}`)
