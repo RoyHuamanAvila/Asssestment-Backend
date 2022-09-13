e7d384b49c280ade1f3321948e1d517224e4d38e
Roy <rhavila789@gmail.com>
2022-09-13 16:52:26 -0500

    fix: change route for auth

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
-const auth = require('./api/auth/local/index.js');
+const auth = require('./auth/local/index.js');
 const listFavs = require('./api/listFavs/index.js');
-const { isAuthenticated } = require('./api/auth/local/auth.controller');
+const { isAuthenticated } = require('./auth/local/auth.controller.js');
 
 const routes = function (app) {
     app.use('/auth/local', auth);
